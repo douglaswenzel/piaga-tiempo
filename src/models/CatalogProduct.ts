f10374047cbada408c09583a1f9cd1b6f12f2a82
douglaswenzel <douglas.wenzel@hotmail.com>
2026-08-18 14:15:34 -0300

    feat: adicionar interface CatalogProduct com propriedades essenciais

---
+++ src/models/CatalogProduct.ts
@@ -0,0 +1,9 @@
+export interface CatalogProduct {
+    sku: string;
+    description: string;
+    manufacturer: string;
+    group: string;
+    application: string;
+    price: number;
+    stock: number;
+}
